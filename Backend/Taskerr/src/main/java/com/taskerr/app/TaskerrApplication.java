c09459b07c6a8c426532a9eb1b1b219e89c1bddb
prasannaj-15 <prasanna15j@gmail.com>
2023-03-24 00:34:27 +0530

    Build Basic Structure of Backend

---
+++ Backend/Taskerr/src/main/java/com/taskerr/app/TaskerrApplication.java
@@ -0,0 +1,13 @@
+package com.taskerr.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskerrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskerrApplication.class, args);
+	}
+
+}
